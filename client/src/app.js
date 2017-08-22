aa42e2b3f26c21bde8e285df3c784598198ba484
Nick DiQuattro <ndiquattro@gmail.com>
2017-08-21 22:41:03 -0700

    added team info

--- client/src/app.js
+++ client/src/app.js
@@ -13,11 +13,12 @@ export class App {
     config.addPipelineStep('authorize', AuthorizeStep);
     config.map([
       // Main Nav routes
-      { route: '/', moduleId: './home/index', nav: 0, title: 'Welcome', auth: false },
+      { route: '/', moduleId: './home/index', nav: 0, title: 'Home', auth: false },
       // { route: 'gradebook', moduleId: './gradebook/index', nav: 1, title: 'Gradebook', name: 'gradebook', auth: true},
       { route: 'gradebook/:subject?/:assignment?', moduleId: './gradebook/index', nav: 1, title: 'Gradebook', name: 'gradebook', auth: true, href: 'gradebook'},
       { route: '/contact', moduleId: './home/contact', title: 'Contact', auth: false },
       { route: '/tos_privacy', moduleId: './home/tos_privacy', title: 'Terms of Service & Privacy Policy', auth: false },
+      { route: '/team', moduleId: './home/team', title: 'Team', auth: false },
 
       // Reports
       { route: 'reports', redirect: 'gradebook'},
